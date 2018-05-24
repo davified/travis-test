a978b37a68a812267913d24b79b279741886b3a5
davidtan <davidtan@thoughtworks.com>
2018-05-24 10:02:34 +0800

    .

---
+++ b.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "running b.sh. this should run on 'staging' and 'prod' branch"
---
+++ c.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "running c.sh. this should run only on 'prod' branch"
