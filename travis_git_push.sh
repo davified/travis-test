fe428924923ac20b53e8f279935bcd13533f5597
davidtan <davidtan@thoughtworks.com>
2018-05-25 16:35:34 +0800

    authenticate travis in git

--- travis_git_push.sh
+++ travis_git_push.sh
@@ -5,17 +5,15 @@ set -e
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  echo "setup_git"
 }
 
 git_add_and_commit() {
-  echo "add commit"
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 git_push() {
-  echo "push"
+  git remote set-url origin https://${GH_TOKEN}@github.com/davified/ml-ci-cd-demo.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master 
 }
 
